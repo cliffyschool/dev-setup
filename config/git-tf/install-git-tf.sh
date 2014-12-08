46b8177d88b4362529e51303579a5fa02b59a406
Cliff Freeman <cliffyschool@gmail.com>
2014-12-08 07:32:54 -0600

    added git-tf install steps

---
+++ config/git-tf/install-git-tf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget http://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-      15B7A078A7F4/git-tf-2.0.3.20131219.zip -O git-tf.zip
+unzip git-tf.zip
+sudo mv git-tf* /usr/local/git-tf
+
