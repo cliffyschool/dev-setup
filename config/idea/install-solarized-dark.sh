dd3ea06848d26c2fcd2f50c64a340077a0f77a72
Cliff Freeman <cliffyschool@gmail.com>
2014-12-08 08:39:06 -0600

    added solarized to idea

---
+++ config/idea/install-solarized-dark.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir ~/.idea_cliff
+cd ~/.idea_cliff
+git clone https://github.com/jkaving/intellij-colors-solarized.git
+cp intellij-colors-solarized/Solarized_*.icls ~/.IdeaIC14/config/colors/ 
+
