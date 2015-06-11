42027d4b582d43db554c720f86ef1bd139226b5f
cliffyschool <cliffyschool@gmail.com>
2015-06-10 20:50:21 -0500

    touchpad disabler is now more disablerey

--- config/hardware/disableStupidDellTouchpad.sh
+++ config/hardware/disableStupidDellTouchpad.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ID=`xinput list | grep -Eo 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
+ID=`xinput list | grep -Eo 'Touchpad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
 xinput set-prop $ID "Device Enabled" 0
